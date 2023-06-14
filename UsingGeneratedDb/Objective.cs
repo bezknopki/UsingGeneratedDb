dd71580ded39607832e46315f925ca2466d6d214
Эрнест Терегулов <aim.exe210@gmail.com>
2023-06-14 12:40:05 +0300

    Add project files.

---
+++ UsingGeneratedDb/Objective.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGeneratedDb;
+
+public partial class Objective
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+}
---
+++ UsingGeneratedDb/ObjectivesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UsingGeneratedDb;
+
+public partial class ObjectivesDbContext : DbContext
+{
+    public ObjectivesDbContext()
+    {
+    }
+
+    public ObjectivesDbContext(DbContextOptions<ObjectivesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Objective> Objectives { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=ObjectivesDB; Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
